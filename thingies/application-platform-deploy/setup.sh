aa459aa9e7b409617b653e7769e6b79622438b66
Chris Cheetham <chris@cheetham.com>
2022-03-07 14:51:19 -0500

    Remove superfluous breadcrumbs

--- thingies/application-platform-deploy/setup.sh
+++ thingies/application-platform-deploy/setup.sh
@@ -9,8 +9,6 @@ crumb "installing TAP packages"
 tanzu package install tap -p tap.tanzu.vmware.com -v $TAP_VERSION --values-file $CONFIG_DIR/tap-values.yaml -n tap-install
 crumb "adding accelerators"
 for acc in $TAP_ACCELERATORS; do
-  name=$(basename $acc)
-  crumb " ... $name"
-  tanzu acc create $name --git-repository $acc --git-branch main --interval 15s
+  tanzu acc create $(basename $acc) --git-repository $acc --git-branch main --interval 15s
 done
 catalog application-platform-deploy
