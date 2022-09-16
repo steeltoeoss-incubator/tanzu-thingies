8da8bf4800a048f0ef505bd933ef7e379a2f5ed9
Chris Cheetham <chris@cheetham.com>
2022-09-16 13:23:59 -0400

    feat: add prereq installer (darwin)

---
+++ bin/prereq-install.ps1
@@ -0,0 +1,19 @@
+#!/usr/bin/env pwsh -NoProfile
+
+. "$PSScriptRoot/../etc/config.ps1"
+
+$prereq_list = "$Env:CONFIG_DIR/prereqs-$Env:PLATFORM.ps1"
+$prereq_script = "$Env:LIBEXEC_DIR/prereq-install-$Env:PLATFORM.ps1"
+
+if (!(Test-Path "$prereq_script"))
+{
+    Die "no prereqs configured for platform: $Env:PLATFORM"
+}
+
+if (!(Test-Path "$prereq_script"))
+{
+    Die "no prereq installer for platform: $Env:PLATFORM"
+}
+
+. "$prereq_list"
+Invoke-Expression $prereq_script
