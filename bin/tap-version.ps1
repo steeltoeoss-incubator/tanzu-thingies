859876ce0932091af8e3991b2d7e81c3cd32e201
Chris Cheetham <chris@cheetham.com>
2022-09-16 13:11:48 -0400

    feat: remove quotes from version output

--- bin/tap-version.ps1
+++ bin/tap-version.ps1
@@ -4,4 +4,5 @@
 
 . "$PSScriptRoot/../etc/config.ps1"
 
-kubectl get packageinstall -n $Env:TAP_NAMESPACE tap -o json | jq .status.version
+$version = kubectl get packageinstall -n $Env:TAP_NAMESPACE tap -o json | jq .status.version
+$version -Replace '"', ""
