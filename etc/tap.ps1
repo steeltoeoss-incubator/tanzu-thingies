2b87b35485cab0d2c4acf64640e865e7d110237d
Chris Cheetham <chris@cheetham.com>
2022-10-12 08:57:29 -0400

    feat: bump Cluster Essentials to 1.3.0

--- etc/tap.ps1
+++ etc/tap.ps1
@@ -8,7 +8,7 @@ $TapDomain = "lvh.me"
 
 $TapProfile = "$ConfigDir/tap-values.yaml"
 
-$TanzuClusterEssentialsVersion = "1.2.0"
+$TanzuClusterEssentialsVersion = "1.3.0"
 $TanzuClusterEssentialsSlug = "tanzu-cluster-essentials"
 $TanzuClusterEssentialsDist = "tanzu-cluster-essentials-$PlatformName-amd64-$TanzuClusterEssentialsVersion.tgz"
 $TanzuClusterEssentialsBundle = "registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:e00f33b92d418f49b1af79f42cb13d6765f1c8c731f4528dfff8343af042dc3e"
