a9d80ad008ff7b41f17ae5876eb5e41324cdab3d
Chris Cheetham <chris@cheetham.com>
2023-02-17 08:42:48 -0500

    feat: bump TAP to 1.5.0-build.23

--- etc/tap.ps1
+++ etc/tap.ps1
@@ -1,4 +1,4 @@
-$TapVersion = "1.5.0-build.17"
+$TapVersion = "1.5.0-build.23"
 $TapCliVersion = "v0.28.0.2"
 $TapSlug = "tanzu-application-platform"
 $TapDist = "tanzu-framework-$PlatformName-amd64-$TapVersion.tar"
